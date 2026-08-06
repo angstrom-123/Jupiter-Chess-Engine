9bab928c31a7216a66494f0f3fb92f6ca4f1190c
angstrom-123 <ignacy.wegner+1@gmail.com>
2026-08-06 21:40:45 +0100

    correct magic file path so that I don't get startup lag

--- src/attackTable.cpp
+++ src/attackTable.cpp
@@ -15,7 +15,7 @@ const std::array<int8_t[2], 4> BISHOP_ATTACKS = {{ { 1, 1 }, { -1, 1 }, { 1, -1 
 const std::array<int8_t[2], 4> ROOK_ATTACKS = {{ { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } }};
 const std::array<int8_t[2], 8> KING_ATTACKS = {{ { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 1 }, { -1, 1 }, { 1, -1 }, { -1, -1 } }};
 
-const fs::path MAGIC_FILE_PATH = fs::path("engine") / "magics.bin";
+const fs::path MAGIC_FILE_PATH = "magics.bin";
 
 AttackTable::AttackTable()
 {
